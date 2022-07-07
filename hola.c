35532c4fd3108ab30bb12929f42c2acb7620c043
levelz08 <andressantamariavargas@gmail.com>
2022-07-07 15:00:23 -0600

    Astronauta

--- hola.c
+++ hola.c
@@ -1,3 +1,4 @@
 //Esta es la historia de un astronauta
 //Dicho astronauta se llama Franklin Chang
 //Este astronauta tras haberse esforzado durante decadas
+//Se atoro en un agujero negro del cual no pudo salir 
